a6a8579f7dac525f80be175f1bf6f5c6d12bc12a
unozer <40232537+unozer@users.noreply.github.com>
2025-05-16 17:37:00 +0200

    test pipe

---
+++ src/app/pipe/list.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'list',
+  standalone: true
+})
+export class ListPipe implements PipeTransform {
+
+  transform(value: string): string[] {
+    return value.split(',');
+  }
+}
